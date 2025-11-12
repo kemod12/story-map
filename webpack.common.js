bec0ad0366ba573c7d4df7b2c7f8f03e9df06233
kemod12 <your-email@example.com>
2025-11-12 23:54:16 +0700

    Perbaikan konfigurasi webpack untuk GitHub Pages

--- webpack.common.js
+++ webpack.common.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
@@ -9,44 +10,62 @@ const publicPath = process.env.NODE_ENV === 'production'
 
 module.exports = {
   entry: {
-    app: path.resolve(__dirname, 'src/scripts/index.js'),
+    main: path.resolve(__dirname, 'src/scripts/index.js'),
     'service-worker': path.resolve(__dirname, 'src/service-worker.js'),
   },
   output: {
-    filename: '[name].js',
+    filename: 'js/[name].[contenthash].js',
     path: path.resolve(__dirname, 'docs'),
     publicPath: publicPath,
+    assetModuleFilename: 'assets/[hash][ext][query]',
+    clean: true,
   },
   module: {
     rules: [
       {
-        test: /\.(png|jpe?g|gif)$/i,
+        test: /\.(png|jpe?g|gif|svg|webp|ico)$/i,
         type: 'asset/resource',
+        generator: {
+          filename: 'images/[hash][ext][query]'
+        }
       },
       {
-        test: /\.css$/i,
-        use: [
-          {
-            loader: 'style-loader',
-            options: {
-              injectType: 'singletonStyleTag'
-            }
-          },
-          {
-            loader: 'css-loader',
-            options: {
-              sourceMap: true
-            }
-          }
-        ]
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'fonts/[hash][ext][query]'
+        }
       },
     ],
   },
   plugins: [
+    new webpack.ProgressPlugin(),
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'src/index.html'),
-      base: process.env.NODE_ENV === 'production' ? '/story-map/' : '/',
-      publicPath: process.env.NODE_ENV === 'production' ? '/story-map/' : '/',
+      filename: 'index.html',
+      chunks: ['main'],
+      minify: {
+        collapseWhitespace: true,
+        removeComments: true,
+        removeRedundantAttributes: true,
+        removeScriptTypeAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        useShortDoctype: true
+      },
+      base: publicPath,
+      publicPath: publicPath,
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'src/public'),
+          to: path.resolve(__dirname, 'docs'),
+          globOptions: {
+            ignore: ['**/index.html']
+          },
+          noErrorOnMissing: true,
+        },
+      ],
     }),
     new CopyWebpackPlugin({
       patterns: [
--- webpack.prod.js
+++ webpack.prod.js
@@ -2,44 +2,66 @@ const common = require('./webpack.common.js');
 const { merge } = require('webpack-merge');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
-  plugins: [
-    new CleanWebpackPlugin({
-      cleanOnceBeforeBuildPatterns: ['**/*', '!CNAME', '!.nojekyll'],
-      cleanAfterEveryBuildPatterns: ['!assets/**/*'],
-      verbose: true,
-      dry: false,
-      cleanStaleWebpackAssets: true,
-      protectWebpackAssets: false
-    })
-  ],
+  devtool: 'source-map',
   module: {
     rules: [
       {
-        test: /\.css$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader',
-        ],
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
+        test: /\.css$/i,
         use: [
           {
-            loader: 'babel-loader',
+            loader: MiniCssExtractPlugin.loader,
             options: {
-              presets: ['@babel/preset-env'],
+              publicPath: '../',
             },
           },
+          'css-loader',
         ],
       },
     ],
   },
   plugins: [
-    new CleanWebpackPlugin(),
-    new MiniCssExtractPlugin(),
+    new CleanWebpackPlugin({
+      cleanOnceBeforeBuildPatterns: ['**/*', '!CNAME', '!.nojekyll', '!**/assets/**'],
+      verbose: true,
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'styles/[name].[contenthash].css',
+      chunkFilename: 'styles/[id].[contenthash].css',
+    }),
   ],
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        parallel: true,
+        terserOptions: {
+          output: {
+            comments: false,
+          },
+        },
+        extractComments: false,
+      }),
+      new CssMinimizerPlugin(),
+    ],
+    splitChunks: {
+      chunks: 'all',
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all',
+        },
+      },
+    },
+  },
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000,
+  },
 });
